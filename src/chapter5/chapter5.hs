a5e7422dbb9df3f48381c44ef9b1487dc5be47f1
준규 <vpark45@gmail.com>
2022-04-06 23:54:32 +0900

    Partial application

---
+++ src/chapter5/chapter5.hs
@@ -0,0 +1,11 @@
+addStuff :: Integer -> Integer -> Integer
+addStuff a b = a + b + 5
+
+addTen = addStuff 5
+
+fifteen = addTen 5
+
+subtractStuff :: Integer -> Integer -> Integer
+subtractStuff x y = x - y - 10
+
+subtractOne = subtractStuff 1
