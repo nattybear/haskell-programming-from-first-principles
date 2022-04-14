c1e017e76902d99da241a0be1a1c3160ab0ebc35
준규 <vpark45@gmail.com>
2022-04-15 07:32:50 +0900

    Add isRight function

--- src/chapter7/guard.hs
+++ src/chapter7/guard.hs
@@ -8,3 +8,9 @@ bloodNa x
   | x < 135   = "too low"
   | x > 145   = "too high"
   | otherwise = "just right"
+
+isRight :: (Num a, Eq a)
+        => a -> a -> a -> String
+isRight a b c
+  | a^2 + b^2 == c^2 = "RIGHT ON"
+  | otherwise        = "not right"
