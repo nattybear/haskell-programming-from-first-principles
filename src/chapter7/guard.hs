02ef238c22143214f7f046194b923759043cd203
준규 <vpark45@gmail.com>
2022-04-15 07:36:11 +0900

    Add avgGrade function

--- src/chapter7/guard.hs
+++ src/chapter7/guard.hs
@@ -22,3 +22,13 @@ dogYrs x
   | x <= 2    = x * 12
   | x <= 4    = x * 8
   | otherwise = x * 6
+
+avgGrade :: (Fractional a, Ord a)
+         => a -> Char
+avgGrade x
+  | y >= 0.9  = 'A'
+  | y >= 0.8  = 'B'
+  | y >= 0.7  = 'C'
+  | y >= 0.59 = 'D'
+  | y < 0.59  = 'F'
+  where y = x / 100
