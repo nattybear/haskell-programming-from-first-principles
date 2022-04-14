8193b47ae24469be3e4c903b65af71cdcbf317ab
준규 <vpark45@gmail.com>
2022-04-14 12:31:37 +0900

    Exrcises: Grab bag

--- src/chapter7/chapter7.hs
+++ src/chapter7/chapter7.hs
@@ -6,3 +6,12 @@ bindExp x =
   let x = 10; y = 5 in
     "the integer was: " ++ show x
     ++ " and y was: " ++ show y
+
+addOneIfOdd n = case odd n of
+  True -> f n
+  False -> n
+  where f = \x -> x + 1
+
+addFive = \x -> \y -> (if x > y then y else x) + 5
+
+mflip f x y = f y x
