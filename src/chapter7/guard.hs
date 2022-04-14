fe903f5ff89800df72100f230e2d125ab075f9b2
준규 <vpark45@gmail.com>
2022-04-15 07:30:55 +0900

    Add bloodNa function

--- src/chapter7/guard.hs
+++ src/chapter7/guard.hs
@@ -2,3 +2,9 @@ myAbs :: Integer -> Integer
 myAbs x
   | x < 0     = (-x)
   | otherwise = x
+
+bloodNa :: Integer -> String
+bloodNa x
+  | x < 135   = "too low"
+  | x > 145   = "too high"
+  | otherwise = "just right"
