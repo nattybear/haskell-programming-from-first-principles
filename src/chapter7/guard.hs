340828f4a584f739cc558dc0d1c29c613bc84ce3
준규 <vpark45@gmail.com>
2022-04-15 07:33:57 +0900

    Add dogYrs function

--- src/chapter7/guard.hs
+++ src/chapter7/guard.hs
@@ -14,3 +14,11 @@ isRight :: (Num a, Eq a)
 isRight a b c
   | a^2 + b^2 == c^2 = "RIGHT ON"
   | otherwise        = "not right"
+
+dogYrs :: Integer -> Integer
+dogYrs x
+  | x <= 0    = 0
+  | x <= 1    = x * 15
+  | x <= 2    = x * 12
+  | x <= 4    = x * 8
+  | otherwise = x * 6
