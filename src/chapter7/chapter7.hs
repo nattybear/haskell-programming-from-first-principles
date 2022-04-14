085ff012015e06b03bc7ca06e4eba72859aae08a
준규 <vpark45@gmail.com>
2022-04-14 12:11:01 +0900

    Look at a case of shadowing

--- src/chapter7/chapter7.hs
+++ src/chapter7/chapter7.hs
@@ -3,6 +3,6 @@ addOne x = x + 1
 
 bindExp :: Integer -> String
 bindExp x =
-  let y = 5 in
-  "the integer was: " ++ show x
-  ++ " and y was: " ++ show y
+  let x = 10; y = 5 in
+    "the integer was: " ++ show x
+    ++ " and y was: " ++ show y
