c80cb87044b214b14b32f8a5ece0ebe6e0d7de99
준규 <vpark45@gmail.com>
2022-04-30 01:35:39 +0900

    Zipping exercises 1

--- src/chapter9/chapter9.hs
+++ src/chapter9/chapter9.hs
@@ -2,3 +2,8 @@ import PoemLines
 
 myWords :: String -> [String]
 myWords = split ' '
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _      []     = []
+zip' []     _      = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
