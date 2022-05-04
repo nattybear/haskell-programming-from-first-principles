a5b2c874923267556ecec9d50c7d04ed82f249e0
준규 <vpark45@gmail.com>
2022-05-04 10:20:05 +0900

    Add seekritFunc function

--- src/chapter10/chapter10.hs
+++ src/chapter10/chapter10.hs
@@ -6,3 +6,8 @@ verbs = ["bite", "hug"]
 
 foo :: [(String, String, String)]
 foo = [(a,b,c) | a <- nouns, b <- verbs, c <- nouns]
+
+seekritFunc :: String -> Int
+seekritFunc x =
+  div (sum (map length (words x)))
+      (length (words x))
