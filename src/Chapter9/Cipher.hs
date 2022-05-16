cb55713c05eaea0b737688248dba600e92e1af22
준규 <vpark45@gmail.com>
2022-05-16 13:13:03 +0900

    Implement Vigenere cipher

--- src/Chapter9/Cipher.hs
+++ src/Chapter9/Cipher.hs
@@ -1,8 +1,9 @@
-module Cipher where
+module Chapter9.Cipher where
 
 caesar :: Int -> Char -> Char
+caesar _ ' ' = ' '
 caesar n c = alphas !! index'
-  where alphas = ['a'..'z']
+  where alphas = ['A'..'Z']
         pairs = zip alphas [0..]
         index = snd . head . filter (\(a, b) -> a == c) $ pairs
         index' = (index + n) `mod` 26
