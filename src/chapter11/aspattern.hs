ce657799fd49ba48e3a48d02759a6c5f708e18ff
준규 <vpark45@gmail.com>
2022-05-16 20:36:25 +0900

    Implement capitalizeParagraph function

--- src/chapter11/aspattern.hs
+++ src/chapter11/aspattern.hs
@@ -14,3 +14,16 @@ capitalizeWords s = map (\w -> (w, capitalizeWord w)) s'
 
 capitalizeWord :: String -> String
 capitalizeWord (x:xs) = toUpper x : xs
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph = init . mconcat . insertDot . map capitalizeWord . split
+
+split :: String -> [String]
+split "" = []
+split s  = word : split (drop 2 rest)
+  where word = takeWhile (/= '.') s
+        rest = dropWhile (/= '.') s
+
+insertDot:: [String] -> [String]
+insertDot []     = []
+insertDot (x:xs) = x : ". " : insertDot xs
