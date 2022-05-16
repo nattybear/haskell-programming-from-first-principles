831aad0d306a0b9eebb9f7b80ace103c017f97b4
준규 <vpark45@gmail.com>
2022-05-16 20:10:35 +0900

    Implement capitalizeWords function

--- src/chapter11/aspattern.hs
+++ src/chapter11/aspattern.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 isSubseqOf :: Eq a => [a] -> [a] -> Bool
 isSubseqOf []         []     = True
 isSubseqOf _          []     = False
@@ -5,3 +7,10 @@ isSubseqOf []         _      = True
 isSubseqOf xs'@(x:xs) (y:ys) = if x == y
                                then isSubseqOf xs  ys
                                else isSubseqOf xs' ys
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords s = map (\w -> (w, capitalizeWord w)) s'
+  where s' = words s
+
+capitalizeWord :: String -> String
+capitalizeWord (x:xs) = toUpper x : xs
