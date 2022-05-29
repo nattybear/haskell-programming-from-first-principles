c6b2313c13e93625805de871714008d9b8e60a71
Vincent <vpark45@gmail.com>
2022-05-29 11:31:41 +0000

    Rewrite Person using Either

--- src/chapter12/person.hs
+++ src/chapter12/person.hs
@@ -3,10 +3,11 @@ type Age = Integer
 
 data Person = Person Name Age deriving Show
 
-mkPerson :: Name -> Age -> Maybe Person
+mkPerson :: Name -> Age -> Either PersonInvalid Person
 mkPerson name age
-  | name /= "" && age >= 0 = Just $ Person name age
-  | otherwise              = Nothing
+  | name /= "" && age >= 0 = Right $ Person name age
+  | name == ""             = Left NameEmpty
+  | otherwise              = Left AgeTooLow
 
 data PersonInvalid = NameEmpty
                    | AgeTooLow
