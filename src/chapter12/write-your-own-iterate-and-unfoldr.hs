a89d39c0c32229aa80eb3851adfc87b706753534
Vincent <vpark45@gmail.com>
2022-05-30 22:39:21 +0000

    Add myUnfoldr function

--- src/chapter12/write-your-own-iterate-and-unfoldr.hs
+++ src/chapter12/write-your-own-iterate-and-unfoldr.hs
@@ -1,3 +1,8 @@
 myIterate :: (a -> a) -> a -> [a]
 myIterate f x = y : myIterate f y
   where y = f x
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f x = case f x of
+                  Nothing     -> []
+                  Just (y, z) -> y : myUnfoldr f z
