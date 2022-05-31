6406f97a25a7aa58b1c7a1ec2303606b0ecf7693
Vincent <vpark45@gmail.com>
2022-05-31 17:57:00 +0000

    Add treeBuild function

--- src/chapter12/tree.hs
+++ src/chapter12/tree.hs
@@ -8,3 +8,8 @@ unfold f v = case f v of
                Just (x,y,z) -> Node l y r
                  where l = unfold f x
                        r = unfold f z
+
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n = unfold (\x -> if x == n
+                            then Nothing
+                            else Just (x+1, x, x+1)) 0
