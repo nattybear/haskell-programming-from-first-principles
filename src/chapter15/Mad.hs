c3df7666592680b7a94758964829f1e7a08a1c6d
Vincent <vpark45@gmail.com>
2022-07-10 12:39:02 +0000

    Rewrite it using mconcat

--- src/chapter15/Mad.hs
+++ src/chapter15/Mad.hs
@@ -8,7 +8,12 @@ type Noun        = String
 type Exclamation = String
 
 madlibbin' :: Exclamation -> Adverb -> Noun -> Adjective -> String
-madlibbin' e adv noun adj = e    <> "! he said " <>
-                            adv  <> " as he jumped into his car " <>
-                            noun <> " and drove off with his " <>
-                            adj  <> " wife."
+madlibbin' e adv noun adj = mconcat [ e
+                                    , "! he said "
+                                    , adv
+                                    , " as he jumped into his car "
+                                    , noun
+                                    , " and drove off with his "
+                                    , adj
+                                    , " wife."
+                                    ]
