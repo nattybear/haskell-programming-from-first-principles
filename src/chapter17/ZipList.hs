75916d1b8982dd532de294fbc08dcf608a16def2
Vincent <vpark45@gmail.com>
2022-08-14 02:31:23 +0000

    Add exercise for ZipList Monoid

--- src/chapter17/ZipList.hs
+++ src/chapter17/ZipList.hs
@@ -1,5 +1,7 @@
 module ZipList where
 
+import Control.Applicative
+import Data.Monoid
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
@@ -20,6 +22,14 @@ instance Eq a => EqProp (ZipList' a) where
       ys' = let (ZipList' l) = ys
             in  take 3000 l
 
+instance Semigroup a
+      => Semigroup (ZipList' a) where
+  (<>) = liftA2 (<>)
+
+instance Monoid a
+      => Monoid (ZipList' a) where
+  mempty = pure mempty
+
 instance Functor ZipList' where
   fmap f (ZipList' xs)
     = ZipList' $ fmap f xs
@@ -36,4 +46,7 @@ trigger :: ZipList' SSI
 trigger = undefined
 
 main :: IO ()
-main = quickBatch (applicative trigger)
+main = do
+  -- quickBatch (applicative trigger)
+  let zl = ZipList' [1 :: Sum Int]
+  quickBatch (monoid zl)
