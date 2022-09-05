2a98cccb622bbbeab18a236282b2157403cce3b1
Vincent <vpark45@gmail.com>
2022-09-05 22:22:47 +0000

    Rewrite using dlist

--- src/chapter23/FizzBuzz.hs
+++ src/chapter23/FizzBuzz.hs
@@ -2,6 +2,7 @@ module FizzBuzz where
 
 import Control.Monad
 import Control.Monad.Trans.State
+import qualified Data.DList as DL
 
 fizzBuzz :: Integer -> String
 fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
@@ -11,15 +12,21 @@ fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
 
 fizzbuzzList :: [Integer] -> [String]
 fizzbuzzList list =
-  execState (mapM_ addResult list) []
+  let dlist =
+        execState (mapM_ addResult list)
+                  DL.empty
+     -- convert back to normal list
+  in DL.apply dlist []
 
-addResult :: Integer -> State [String] ()
+addResult :: Integer
+          -> State (DL.DList String) ()
 addResult n = do
   xs <- get
   let result = fizzBuzz n
-  put (result : xs)
+  -- snoc appends to the end, unlike
+  -- cons which adds to the front
+  put (DL.snoc xs result)
 
 main :: IO ()
 main =
-  mapM_ putStrLn $
-    reverse $ fizzbuzzList [1..100]
+  mapM_ putStrLn $ fizzbuzzList [1..100]
