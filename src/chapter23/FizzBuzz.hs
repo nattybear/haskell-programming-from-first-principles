9856c2216edbbd4c20475af23e57c76a29e739cb
Vincent <vpark45@gmail.com>
2022-09-05 12:09:51 +0000

    Rewrite using State

--- src/chapter23/FizzBuzz.hs
+++ src/chapter23/FizzBuzz.hs
@@ -1,11 +1,25 @@
 module FizzBuzz where
 
-fizzbuzz :: Integer -> String
-fizzbuzz n | n `mod` 15 == 0 = "FizzBuzz"
+import Control.Monad
+import Control.Monad.Trans.State
+
+fizzBuzz :: Integer -> String
+fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
            | n `mod` 5  == 0 = "Buzz"
            | n `mod` 3  == 0 = "Fizz"
            | otherwise       = show n
 
+fizzbuzzList :: [Integer] -> [String]
+fizzbuzzList list =
+  execState (mapM_ addResult list) []
+
+addResult :: Integer -> State [String] ()
+addResult n = do
+  xs <- get
+  let result = fizzBuzz n
+  put (result : xs)
+
 main :: IO ()
 main =
-  mapM_ (putStrLn . fizzbuzz) [1..100]
+  mapM_ putStrLn $
+    reverse $ fizzbuzzList [1..100]
