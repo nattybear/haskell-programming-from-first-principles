01a2f036048ace29c0789b373fd6213dc88ab947
Vincent <vpark45@gmail.com>
2022-09-05 22:25:56 +0000

    Using Foldable dlist

--- src/chapter23/FizzBuzz.hs
+++ src/chapter23/FizzBuzz.hs
@@ -10,21 +10,16 @@ fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
            | n `mod` 3  == 0 = "Fizz"
            | otherwise       = show n
 
-fizzbuzzList :: [Integer] -> [String]
+fizzbuzzList :: [Integer]
+             -> DL.DList String
 fizzbuzzList list =
-  let dlist =
-        execState (mapM_ addResult list)
-                  DL.empty
-     -- convert back to normal list
-  in DL.apply dlist []
+  execState (mapM_ addResult list) DL.empty
 
 addResult :: Integer
           -> State (DL.DList String) ()
 addResult n = do
   xs <- get
   let result = fizzBuzz n
-  -- snoc appends to the end, unlike
-  -- cons which adds to the front
   put (DL.snoc xs result)
 
 main :: IO ()
