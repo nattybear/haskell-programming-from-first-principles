9dbf653377a5eb799a0f633918a87df6e9a0297b
vincent <vpark45@gmail.com>
2022-10-21 23:14:32 +0900

    Implement some instances

--- src/chapter26/StateT.hs
+++ src/chapter26/StateT.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs  #-}
 
 module StateT where
 
@@ -10,4 +10,38 @@ instance Functor m =>
   fmap :: (a -> b)
        -> StateT s m a
        -> StateT s m b
-  fmap f (StateT smas) = undefined
+  fmap f (StateT sma) =
+    StateT $ \s ->
+      let ma = sma s
+      in  fmap (\(a, s') -> (f a, s')) ma
+
+instance Monad m =>
+         Applicative (StateT s m) where
+  pure :: a -> StateT s m a
+  pure a = StateT $ \s -> pure (a, s)
+
+  (<*>) :: StateT s m (a -> b)
+        -> StateT s m a
+        -> StateT s m b
+  StateT smfs <*> StateT smas =
+    StateT $ \s ->
+      let mfs = smfs s
+      in  do
+        (f, s') <- mfs
+        let mas = smas s'
+        (a, s'') <- mas
+        return (f a, s'')
+
+instance Monad m =>
+         Monad (StateT s m) where
+  return :: a -> StateT s m a
+  return = pure
+
+  (>>=) :: StateT s m a
+        -> (a -> StateT s m b)
+        -> StateT s m b
+  StateT smas >>= f =
+    StateT $ \s -> do
+      (a, s') <- smas s
+      let smbs = runStateT (f a)
+      smbs s'
