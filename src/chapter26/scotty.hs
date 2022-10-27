fe2b8fa4530807de495b117c6ce918ba8aa193f0
vincent <vpark45@gmail.com>
2022-10-27 20:58:59 +0900

    Rewrite using liftIO

--- src/chapter26/scotty.hs
+++ src/chapter26/scotty.hs
@@ -2,28 +2,15 @@
 
 module Scotty where
 
-import Control.Monad
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Control.Monad.Trans.Except
-import Control.Monad.Trans.State.Strict
-  hiding (get)
-import Control.Monad.Trans.Reader
 import Data.Monoid (mconcat)
 import Web.Scotty
-import Web.Scotty.Internal.Types
-  (ActionT(..))
 
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
-
-    (ActionT
-     . (ExceptT . liftM Right)
-     . ReaderT . const
-     . \m -> StateT (\s -> do
-                        a <- m
-                        return (a, s))
-     ) (putStrLn "hello")
+    liftIO  (putStrLn "hello")
 
     html $
       mconcat [ "<h1>Scotty, "
